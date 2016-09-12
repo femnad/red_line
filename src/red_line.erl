8fbe54995950d60160fef8ad25203f4b5ea03c47
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2016-09-11 20:58:19 -0400

    Handle Full Battery Status

--- src/red_line.erl
+++ src/red_line.erl
@@ -5,6 +5,7 @@
 
 -define(CRITICAL_PERCENT, 13).
 -define(NORMAL_SLEEP, 60 * 1000).
+-define(PID_FILE, "/tmp/red_line.pid").
 -define(WARNING_SLEEP, 5 * 1000).
 
 %%====================================================================
@@ -13,7 +14,7 @@
 
 %% escript Entry point
 main(_) ->
-    check_loop(),
+    start(),
     erlang:halt(0).
 
 %%====================================================================
@@ -30,8 +31,13 @@ parse_acpi_output(AcpiOutput) ->
             [_, _, Status] = string:tokens(BatteryAndStatus, " :"),
             {Percent, _} = string:to_integer(Percentage),
             {Status, Percent};
-        [_, _] ->
-            unknown
+        [_, Percentage] ->
+            case string:strip(Percentage) of
+                "100%" ->
+                    full;
+                _ ->
+                    unknown
+            end
     end.
 
 handle_battery_status(Status, _) when Status == "Charging" ->
@@ -45,8 +51,11 @@ handle_battery_status(_, Percent) ->
             ok
     end.
 
+get_acpi_info() ->
+    os:cmd("acpi").
+
 check_loop() ->
-    AcpiResult = os:cmd("acpi"),
+    AcpiResult = get_acpi_info(),
     case parse_acpi_output(AcpiResult) of
         unknown ->
             % Hope for the best and try again
@@ -55,3 +64,48 @@ check_loop() ->
             handle_battery_status(Status, Percent)
     end,
     check_loop().
+
+process_exists_with_saved_pid() ->
+    {ok, File} = file:open(?PID_FILE, [read]),
+    {ok, Line} = file:read_line(File),
+    SavedPid = string:tokens(Line, "\n"),
+    PsCommand = lists:flatten(io_lib:format("ps -f --pid ~s", [SavedPid])),
+    PsResult = os:cmd(PsCommand),
+    case string:tokens(PsResult, "\n") of
+        [_Header, _Process] ->
+            % If a process with that PID exists it must be red_line, right?
+            true;
+        [_Header] ->
+            false
+    end.
+
+save_pid_to_file() ->
+    {ok, File} = file:open(?PID_FILE, [write, exclusive]),
+    PidLine = lists:flatten(io_lib:format("~s~n", [os:getpid()])),
+    file:write(File, PidLine),
+    file:close(File).
+
+start_loop() ->
+    save_pid_to_file(),
+    check_loop().
+
+start() ->
+    case file:open(?PID_FILE, read) of
+        {error, _} ->
+            start_loop();
+        {ok, _} ->
+            case process_exists_with_saved_pid() of
+                true ->
+                    io:format("A red_line process is already active~n");
+                false ->
+                    %% The PID file is stale
+                    file:delete(?PID_FILE),
+                    start_loop()
+            end
+    end.
+
+-ifdef(TEST).
+acpi_parse_test() ->
+    FullAcpi = "Battery 0: Full, 100%\n",
+    true = parse_acpi_output(FullAcpi) == full.
+-endif.
